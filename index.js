cde85320bb824382228fd4c8f937c45100df0cd6
unknown <hominhchanh08@gmail.com>
2021-06-02 11:19:25 +0700

    Commit

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* SCROLL ANIMATIONS SLIDER - INTRO - HOME  */
 const tl = gsap.timeline({
     defaults: { ease: "power1.out" } 
 });
@@ -15,6 +16,7 @@ tl.from(".line span", 1.8, {
         amount: 0.4,
     },
 })
+/* SCROLL ANIMATIONS MENU */
 var tl1 = new TimelineMax({ paused: true });
 tl1.to(".menu", 2, {
     top: "0%",
@@ -49,7 +51,7 @@ $(document).on("click", ".menu-close", function () {
 $(document).on("click", "a", function () {
     tl1.reversed(!tl1.reversed());
 });
-
+/* CUSTOMIZE MOUSE CURSOR */
 
 var cursor = $(".cursor"),
     follower = $(".cursor-follower");
@@ -77,41 +79,47 @@ $(document).on("mousemove", function (e) {
     mouseX = e.pageX;
     mouseY = e.pageY;
 });
-$("header").on("mouseenter", function () {
-    cursor.addClass("active");
-    follower.addClass("active");
-});
-$("header").on("mouseleave", function () {
-    cursor.removeClass("active");
-    follower.removeClass("active");
-});
-$("div").on("mouseenter", function () {
-    cursor.addClass("active");
-    follower.addClass("active");
-});
-$("div").on("mouseleave", function () {
-    cursor.removeClass("active");
-    follower.removeClass("active");
-});
+// $("header").on("mouseenter", function () {
+//     cursor.addClass("active");
+//     follower.addClass("active");
+// });
+// $("header").on("mouseleave", function () {
+//     cursor.removeClass("active");
+//     follower.removeClass("active");
+// });
+// $("div").on("mouseenter", function () {
+//     cursor.addClass("active");
+//     follower.addClass("active");
+// });
+// $("div").on("mouseleave", function () {
+//     cursor.removeClass("active");
+//     follower.removeClass("active");
+// });
+
+
+/* SCROLL ANIMATIONS FOR SECTIONS TWO */
 
 const sroll = gsap.timeline({
     scrollTrigger: {
         trigger: '.section-two',
         start: 'top 90%',
         end:'bottom bottom',
-        markers: true,
     }
 })
-sroll.from('.section-two',2.5,{ opacity: 0})
 
+
+sroll.from('.section-two',2.5,{ opacity: 0})
 sroll.from('.rectangle',{ opacity: 0, duration: 1}, '=-2')
 sroll.from('.text-sub',{y:'80', opacity: 0 , duration: 1},'-=1.5')
 sroll.from('.text-title',{y:'100%', opacity: 0 , duration: 1},'.5','-=2')
 sroll.from('.text-desc',{y:'100%', opacity: 0 , duration: 1},'1','-=1.5')
 sroll.from('.text-year',{y:'100%', opacity: 0 , duration: 1},'.5','-=1')
-sroll.from('.image-one', { opacity: 0, y: '-500', duration:1})
+sroll.from('.image-one', { opacity: 0, y: '-500', duration:1},'2','-=1.5')
 sroll.from('.image-two',{ opacity: 0, x: '-350', duration:1},'2','-=1')
 sroll.from('.image-three',{ opacity: 0, x: '350', duration:1},'2','-=1')
+
+
+/* SCROLL HORIZONTAL SECTIONS */
 var sections = gsap.utils.toArray(".panel");
 const scrollhrz = gsap.timeline();
 scrollhrz.to(sections, {
@@ -125,3 +133,72 @@ scrollhrz.to(sections, {
     end: () => "+=" + document.querySelector(".container").offsetWidth
   }
 });
+
+/* SCROLL ANIMATION SECTIONS THREE */
+const scroll = gsap.timeline({
+    scrollTrigger: {
+        trigger: '.section-three',
+        start: 'top 80%',
+        end:'bottom bottom',
+    }
+})
+scroll.from(".img-circle",{
+    opacity: 0,
+})
+scroll.from(".text-subtitle",1.5,{
+    opacity: 0,
+    y:-100,
+},'-=1.5')
+scroll.from(".text-heading",1.2,{
+    opacity: 0,
+    y:-50,
+    duration:1.2,
+},'=-0.8')
+scroll.from(".hr", 1.8,{
+    opacity:0,
+    x:'-50%',
+    delay:1.5,
+},'=-2.5')
+scroll.from(".hrtwo", 1.6,{
+    opacity:0,
+    x:'-50%',
+    delay:1.3,
+    stagger: {
+        amount: 0.6
+    }
+},'=-2.5')
+scroll.from(".text-text span", 1.6, {
+    y: 400,
+    ease: "power4.out",
+    skewY: 10,
+    stagger: {
+        amount: 0.4,
+    },
+},'=-2')
+scroll.from(".text-number", 2,{
+    opacity: 0,
+    duration: 1,
+    stagger: {
+        amount: 0.5,
+    }
+},'-=1')
+
+const scrollthree = gsap.timeline({
+    scrollTrigger: {
+        trigger: '.section-four',
+        start: 'top 80%',
+        end:'bottom bottom',
+    }
+})
+scrollthree.from('.text-about span', 1.6,{
+    y: 600,
+    ease: "power4.out",
+    skewY: 10,
+    stagger: {
+        amount: 0.4,
+    },
+},'-=2')
+scrollthree.from(".hr-about", 1.6,{
+    opacity: 0,
+    x:'-50%'
+},'=-1.6')
